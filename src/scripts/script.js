81a5be9ba50f23e6b3cd742ad9cfef9ad620957c
Nick Rabone <n.rabone72@aol.com>
2024-05-01 22:58:38 +0100

    Added additional CSS to format widgets correctly.

---
+++ src/scripts/script.js
@@ -0,0 +1,24 @@
+const toggleNav = document.querySelector('.toggle-nav')
+const toggleItems = document.querySelectorAll('.toggle-item')
+
+const mapsChart = document.querySelector('maps-chart')
+const detailedChart = document.querySelector('.detailed-chart')
+const navItems = document.querySelector('.nav-items')
+
+toggleNav.onclick = () => {
+    navItems.classList.toggle('show')
+    toggleNav.innerHTML = navItems.classList.contains('show') ? '<i class="fas fa-less-than"></i>' : '<i class="fas fa-greater-than"></i>'
+}
+
+toggleItems.forEach((item) => {
+    item.onclick = (e) => {
+        if (e.target.classList.contains('maps')) {
+            e.target.classList.add('active')
+            e.target.nextElementSibling.classList.remove('active')
+        }
+        if (e.target.classList.contains('analytics')) {
+            e.target.classList.add('active')
+            e.target.previousElementSibling.classList.remove('active')
+        }
+    }
+})
